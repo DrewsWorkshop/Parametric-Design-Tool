ba367d3badd51db214afc4735c69b49e2ace3e76
Andrew Beyer <andrew44b@gmail.com>
2025-09-03 17:49:04 -0400

    Delete ParametricDesignTool_V1 directory

--- ParametricDesignTool_V1/src/camera/cameraSetup.py
+++
@@ -1,8 +0,0 @@
-def disable_controls(self):
-    self._dragging = False
-    self._last_mouse = None
-
-def enable_controls(self):
-    # Controls are enabled by default, just ensure dragging is reset
-    self._dragging = False
-    self._last_mouse = None
